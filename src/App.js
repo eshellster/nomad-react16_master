892936b57bdfc5698acdbd09053f9a2461aa314a
eshell <eshellster@gmail.com>
2019-03-12 20:28:08 +0900

    HOC 예제

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { createPortal } from "react-dom";
+
+
+const BoundaryHOC = ProtectedComponet => class Boundary extends Component {
+  state = {
+  hasError: false
+  };
+  componentDidCatch = () => {
+    this.setState({
+      hasError: true
+    });
+  };
+  render() {
+    const { hasError } = this.state;
+    if (hasError) {
+      return <ErrorFallback />;
+    } else {
+      return <ProtectedComponet />;
+    }
+  }
+}
+
+class ErrorMarker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() =>{
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  }
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend}`);
+  }
+}
+
+const PErrorMarker = BoundaryHOC(ErrorMarker)
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchMe"));
+  }
+}
+
+const PPortals = BoundaryHOC(Portals)
+
+const Message = () => "Just touched it"
+
+class ReturnTypes extends Component {
+  render(){
+    return "hello!";
+  }
+}
+
+
+const ErrorFallback = () => " ====== Sorry something went wrong ======";
 
 class App extends Component {
+ 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <ReturnTypes/>
+        <PPortals />
+        <PErrorMarker /> 
+      </Fragment>
     );
   }
 }
 
-export default App;
+export default  BoundaryHOC(App);
